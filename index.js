fc0d8e9d7d5db29e7a49ebe9f13e8118511c045d
Alejandro Cruz <dragex@yahoo.com>
2022-04-25 22:01:57 -0400

    add clear button and double-click to turn pixel white

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ let currentColor = 'blue'
 const painting = document.querySelector('.painting')
 
 painting.addEventListener('click', function(e){
-	e.target.style.backgroundColor = currentColor
+	e.target.style.backgroundColor = currentColor;
+})
+
+painting.addEventListener('dblclick', function(l){
+	l.target.style.backgroundColor = 'white';
 })
 
 const palette = document.querySelector('.palette')
 
 palette.addEventListener('click', function (w){
-	currentColor = w.target.id
+	currentColor = w.target.id;
 })
+
+const button = document.querySelector('.reset')
+let pixels = document.querySelectorAll('.pixel')
+
+button.addEventListener('click', function(){
+	for (let i = 0; i < pixels.length; i++)
+	{
+	pixels[i].style.backgroundColor = "white"
+	}
+})
+
